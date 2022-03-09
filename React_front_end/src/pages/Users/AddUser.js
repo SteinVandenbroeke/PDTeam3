3f932f44d0cbe05bf96f0d5aa4df62c80fe38161
niels <nielsvandenbroeck11@gmail.com>
2022-03-09 22:32:58 +0100

    Add Pofile Pic

--- React_front_end/src/pages/Users/AddUser.js
+++ React_front_end/src/pages/Users/AddUser.js
@@ -1,13 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import {Card, Container, ListGroup, Form, Button,Col,Row} from "react-bootstrap";
+import React, {useEffect} from 'react';
+import {Card, Container, ListGroup, Form, Button, Col, Row, Image} from "react-bootstrap";
 import LogicTable from "../../components/logicTable";
 import {Link} from "react-router-dom";
 import Icon from "react-eva-icons";
+import ProfileImageMenu from "../../components/profileImageMenu";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const AddUsers = () => {
     let date = new Date();
-    const maxDateValue = date.toISOString().split("T")[0];  // convert to ISO string
+    const maxDateValue = date.toISOString().split("T")[0];
+    function setImage(path) {
+        console.log(path)
+    }
     return (
         <div className="App">
             <Container>
@@ -34,15 +41,25 @@ const AddUsers = () => {
                                 <Form.Control type="password" placeholder="Password" />
                             </Row>
                             <Row style={{padding:10}}>
-                           <Col>
-                               <Form.Label>Date of Birth</Form.Label>
-                               <Form.Control style={{ textAlign:"center"}} max={maxDateValue} type="date" name="dob" placeholder="Date of Birth"/>
-                            </Col>
-                            <Col>
-                                <Form.Label>Admin Permission</Form.Label>
-                                <Form.Check/>
-                            </Col>
-
+                                <Col>
+                                   <Form.Label>Date of Birth</Form.Label>
+                                   <Form.Control style={{ textAlign:"center"}} max={maxDateValue} type="date" name="dob" placeholder="Date of Birth"/>
+                                </Col>
+                                <Col>
+                                    <Form.Label onClick={setImage(date)}>Admin Permission</Form.Label>
+                                    <Form.Check/>
+                                </Col>
+                            </Row>
+                            <Row style={{padding:10}}>
+                                <Col>
+                                    <Form.Group controlId="ProfilePic" className="mb-3">
+                                        <Form.Label >Add profile picture</Form.Label>
+                                        <Form.Control  type="file" accept=".png,.jpg"/>
+                                     </Form.Group>
+                                </Col>
+                                <Col xs={2}>
+                                    <Image roundedCircle={true} height={50} src={"https://scontent.fbru1-1.fna.fbcdn.net/v/t1.6435-9/78431746_2293299950961562_7867165954851995648_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=UpMJOOQDA7EAX-FFLhh&_nc_ht=scontent.fbru1-1.fna&oh=00_AT--woVzrD7i2DiqMtz8n0KS3O0dvV2-8lG7QUWQde11eQ&oe=624621AC"}></Image>
+                                </Col>
                             </Row>
                             <Row style={{padding:10}}>
                                 <Button variant="primary" type="submit">
