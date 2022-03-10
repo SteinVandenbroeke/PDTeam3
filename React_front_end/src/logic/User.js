3f6fa03f327eafd54e64ecd89c7a2c6e73d7fd1c
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-03-11 00:15:11 +0100

    User login interface

--- React_front_end/src/logic/User.js
+++ React_front_end/src/logic/User.js
@@ -1,30 +1,74 @@
 import React, { useState } from 'react';
+import { ServerRequest } from './ServerCommunication';
 
-export default class User{
+export class User{
+    /***
+     * Creates a new user
+     */
     constructor() {
         this.isLoggedInV = false;
-        if(window.localStorage.getItem("loggedIn") !== null){
+        this.userToken = null;
+        if(window.localStorage.getItem("authToken") !== null){
             this.isLoggedInV = true;
+            this.userToken = window.localStorage.getItem("authToken");
         }
     }
 
-    async login(email, password){
-        this.isLoggedInV = true;
-        window.localStorage.setItem("loggedIn", "true");
-        console.log("test");
-        return "ok"
+    /***
+     * User login
+     * @param formdata (formdata of the loginForm ("email" and "password" as keys))
+     * @returns if user is succesfully loggedin {Promise<boolean>}
+     */
+    async login(formdata){
+        //this.isLoggedInV = true;
+        //window.localStorage.setItem("loggedIn", "true");
+        try{
+            let request = new ServerRequest();
+            let response = await request.sendPost("login",formdata, false);
+            this.userToken = response.token;
+            window.localStorage.setItem("authToken", this.userToken);
+            this.isLoggedInV = true;
+            return true;
+        }
+        catch(err) {
+            throw err;
+        }
+        return false;
     }
 
+    /***
+     * user logout
+     * @returns {Promise<void>}
+     */
     async logout(){
         this.isLoggedInV = false;
-        window.localStorage.removeItem("loggedIn");
+        window.localStorage.removeItem("authToken");
     }
 
+    /***
+     * Register new user
+     * @param email
+     * @param password
+     * @param username
+     * @returns {Promise<void>}
+     */
     async register(email, password, username){
-
+        //TODO
     }
 
+    /***
+     * Returns if user is loggedin
+     * @returns {boolean|*}
+     */
     isLoggedIn(){
         return this.isLoggedInV;
+    }
+
+    /***
+     *Function that gives the userToken
+     * @returns userToken {null|string|*|?string}
+     */
+    getUserToken(){
+        return this.userToken;
     }
 }
