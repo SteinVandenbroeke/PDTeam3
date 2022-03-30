8ed71fbb13b16159e6fdc3144604a01018290537
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-03-30 13:32:27 +0200

    config file

---
+++ Flask_server/src/ProgDBTutor/config.py
@@ -0,0 +1,5 @@
+config_data = dict()
+config_data['app_name'] = 'ProgDBTutor'
+config_data['dbname'] = 'dbtutor'
+config_data['dbuser'] = 'postgresPython'
+config_data['uri'] = 'postgresql://postgresPython:dbAdmin@localhost:5432/dbtutor'
