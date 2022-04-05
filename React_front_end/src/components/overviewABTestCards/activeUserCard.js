8c0e6494eae4d7e3ce119d791634f3be93718583
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-04-05 16:17:33 +0200

    User card

---
+++ React_front_end/src/components/overviewABTestCards/activeUserCard.js
@@ -0,0 +1,74 @@
+import {Button} from "react-bootstrap";
+import Icon from 'react-eva-icons';
+import {useNavigate } from "react-router-dom";
+import React, {useEffect} from "react";
+import LargeInformationCard from "../largeInformationCard";
+import { Line } from 'react-chartjs-2';
+import {
+Chart as ChartJS,
+CategoryScale,
+LinearScale,
+PointElement,
+LineElement,
+Title,
+Tooltip,
+Legend,
+} from 'chart.js';
+import SmallInformationCard from "../smallInformationCard";
+
+const RevenueCard = (props) => {
+    const navigation = useNavigate();
+    const [labels, Letlabels] = React.useState([]);
+    const [datasets, setDatasets]  = React.useState([]);
+    const [totalUsers, setTotalUsers]  = React.useState(0);
+
+    function processData(begin, end){
+        setDatasets([]);
+        let allData = props.abTestData;
+
+        Letlabels(allData.points.slice(begin, end + 1));
+
+        let data = [];
+        let totalUsersTemp = 0;
+        allData.NotAlgDependent.slice(begin, end + 1).map((value, index) =>{
+                    data.push(value.activeUsersAmount);
+                    totalUsersTemp += value.activeUsersAmount;
+                });
+
+        setTotalUsers(totalUsersTemp);
+        setDatasets(datasets => [...datasets, {
+                  label: "Revenue",
+                  data: data,
+                }]);
+    }
+
+    useEffect(() => {
+        processData(props.startDate,props.endDate);
+    }, [props.abTestData, props.startDate, props.endDate]);
+
+    return (
+        <LargeInformationCard title={"Users"} value={20} tooltip={"Purchases from day x to day y"}>
+            <h5>Total from {props.abTestData.points[props.startDate]} to {props.abTestData.points[props.endDate]}: {totalUsers}</h5>
+            {labels.length < 500 &&
+            <Line options={{
+                  responsive: true,
+                  plugins: {
+                    legend: {
+                      position: 'top',
+                    },
+                    title: {
+                      display: false,
+                    },
+                  },
+                }}
+
+            data={{
+              labels, datasets: datasets
+            }} />}
+            {labels.length >= 500 &&
+                <p style={{color: "red"}}>To many datapoints to show in a graph</p>}
+        </LargeInformationCard>
+        )
+};
+
+export default RevenueCard;
