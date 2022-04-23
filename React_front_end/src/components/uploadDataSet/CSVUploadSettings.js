85a2ae035eef25eef4fc1627cf26bfb1772db5b8
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-04-23 16:36:59 +0200

    Message when needed comums are not connected

--- React_front_end/src/components/uploadDataSet/CSVUploadSettings.js
+++ React_front_end/src/components/uploadDataSet/CSVUploadSettings.js
@@ -6,6 +6,7 @@ import {Link} from "react-router-dom";
 import ConnectComponent from "../ConnectComponent"
 import LogicTable from "../logicTable"
 import { UploadDataSet } from '../../logic/UploadDataSet';
+import {toast} from "react-toastify";
 
 const CSVUploadSettings = (props) => {
 
@@ -21,12 +22,30 @@ const CSVUploadSettings = (props) => {
     }
 
     function nexStep(){
-        /*
+
         console.log(props.csvDbConnections)
-        if("t_dat" in props.csvDbConnections.connections){
-            console.log("ok")
-        }*/
-        props.setCurrentStep(props.currentStep + 1)
+        if("article_id" in props.csvDbConnections.connections &&
+           "customer_id" in props.csvDbConnections.connections &&
+           "price" in props.csvDbConnections.connections &&
+           "t_dat" in props.csvDbConnections.connections) {
+            //interaction
+            props.setCurrentStep(props.currentStep + 1);
+        }
+        else if(!("title" in props.csvDbConnections.database) &&
+                !("description" in props.csvDbConnections.database) &&
+                "customer_id" in props.csvDbConnections.connections){
+            //users
+             props.setCurrentStep(props.currentStep + 1);
+        }
+        else if("article_id" in props.csvDbConnections.connections &&
+           "prod_name" in props.csvDbConnections.connections &&
+           "product_type_name" in props.csvDbConnections.connections) {
+            //articels
+            props.setCurrentStep(props.currentStep + 1);
+        }
+        else{
+            toast.warn("Niet alle nodige columen zijn verbonden!")
+        }
     }
 
     return (
