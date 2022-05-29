61ad73ab7b7edb66862229666ba6b9a30081e417
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-05-29 20:20:35 +0200

    cors origin

--- Flask_server/src/ProgDBTutor/wsgi.py
+++ Flask_server/src/ProgDBTutor/wsgi.py
@@ -2,5 +2,5 @@ from app import app
 #from app import socketio
 
 if __name__ == "__main__":
-    socketio.run(app, cors_allowed_origins="http://team3.ua-ppdb.me")
+    socketio.run(app, debug=DEBUG, cors_allowed_origins="http://team3.ua-ppdb.me")
     #app.run()
