c6170a176ed2f34a8f73e04e85ca681c65bdaf92
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2022-05-30 18:50:26 +0200

    Image sizes

--- React_front_end/src/pages/Page404.js
+++ React_front_end/src/pages/Page404.js
@@ -14,7 +14,7 @@ const Page404 = () => {
                     <Card className={"shadow-lg"} style={{padding: 30}}>
                         <BackButton/>
                         <div style={{textAlign: "center", paddingTop: 40, zIndex: 1}}>
-                            <img src={"/svg/serverDown.svg"} style={{width: "40%"}}/>
+                            <img src={"/svg/serverDown.svg"} style={{width: "20%"}}/>
                             <h1 style={{paddingTop: 40}}>Page not found (404)</h1>
                         </div>
                         <h1 style={{position: "absolute", right: "-5%", top: "12%", transform: "rotate(-90deg)", fontSize: "1000%", zIndex: 0, color:"#3472b510" }}>404</h1>
